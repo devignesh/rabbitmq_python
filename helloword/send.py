643ffc223d2ea8bbf952c3b5804f80d6c7a50a4f
devignesh <vinesh1865@gmail.com>
2019-10-07 12:43:25 +0530

    send msg to queue

--- helloword/send.py
+++ helloword/send.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env_python
 import pika
 connection = pika.BlockingConnection(pika.ConnectionParameters('127.0.0.1:8000'))
 channel = connection.channel()
 channel.queue_declare(queue='vickymsgque')
 
-channel.basic_publish(exchange='', routing_key='msg', body='Hello World!')
-print(" [x] Sent 'hi vicky'")
+channel.basic_publish(exchange='', routing_key='msg', body='hi vicky')
+# print(" [x] Sent 'hi vicky'")
 connection.close()
