59b78863e9ef4b78a5c2ef7f258063e5c4e71428
devignesh <vinesh1865@gmail.com>
2019-10-09 21:14:49 +0530

    receive the msg queue

--- publish/emit_log.py
+++ publish/emit_log.py
@@ -7,7 +7,7 @@ channel = connection.channel()
 
 channel.exchange_declare(exchange='logs', exchange_type='fanout')
 
-message = ' '.join(sys.argv[1:]) or "info: Hello World!"
+message = ' '.join(sys.argv[1:]) or "info: hi vicky"
 channel.basic_publish(exchange='logs', routing_key='', body=message)
 print(" [x] Sent %r" % message)
 connection.close()
