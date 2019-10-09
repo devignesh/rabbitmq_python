c8349b023a19e5247493d5a1cfab73abe89beae1
devignesh <vinesh1865@gmail.com>
2019-10-09 21:17:06 +0530

    receive the msg queue

--- publish/receive_log.py
+++ publish/receive_log.py
@@ -4,14 +4,13 @@ connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.exchange_declare(exchange='logs', exchange_type='fanout')
+channel.exchange_declare(exchange='logs', exchange_type='input')
 
 result = channel.queue_declare(queue='', exclusive=True)
 queue_name = result.method.queue
 
 channel.queue_bind(exchange='logs', queue=queue_name)
 
-print(' [*] Waiting for logs. To exit press CTRL+C')
 
 def callback(ch, method, properties, body):
     print(" [x] %r" % body)
