d267863c99e55712254fea18530b31764d078a1d
devignesh <vinesh1865@gmail.com>
2019-10-09 21:17:37 +0530

    adding msg queue

--- publish/emit_log.py
+++ publish/emit_log.py
@@ -5,7 +5,7 @@ connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.exchange_declare(exchange='logs', exchange_type='fanout')
+channel.exchange_declare(exchange='logs', exchange_type='input')
 
 message = ' '.join(sys.argv[1:]) or "info: hi vicky"
 channel.basic_publish(exchange='logs', routing_key='', body=message)
