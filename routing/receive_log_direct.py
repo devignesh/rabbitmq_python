30a4add3694bf517dee3f963fc09c1750ef28fbe
devignesh <vinesh1865@gmail.com>
2019-10-10 13:25:24 +0530

    receive the msg using routing in rabbitmq

--- routing/receive_log_direct.py
+++ routing/receive_log_direct.py
@@ -19,9 +19,6 @@ for severity in severities:
     channel.queue_bind(
         exchange='direct_logs', queue=queue_name, routing_key=severity)
 
-print(' [*] Waiting for logs. To exit press CTRL+C')
-
-
 def callback(ch, method, properties, body):
     print(" [x] %r:%r" % (method.routing_key, body))
 
