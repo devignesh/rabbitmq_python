54e61164fbfe2e4cb79a0c10995530b1f155916f
devignesh <vinesh1865@gmail.com>
2019-10-10 13:23:34 +0530

    routing in rabbitmq

--- routing/log_direct.py
+++ routing/log_direct.py
@@ -8,7 +8,7 @@ channel = connection.channel()
 channel.exchange_declare(exchange='direct_logs', exchange_type='direct')
 
 severity = sys.argv[1] if len(sys.argv) > 1 else 'info'
-message = ' '.join(sys.argv[2:]) or 'Hello World!'
+message = ' '.join(sys.argv[2:]) or 'Hi vicky'
 channel.basic_publish(
     exchange='direct_logs', routing_key=severity, body=message)
 print(" [x] Sent %r:%r" % (severity, message))
