94fdcb960cc94b2e2b8f0dc958e59356beed46e8
devignesh <vinesh1865@gmail.com>
2019-10-18 22:08:24 +0530

    client send file

--- topics/emit_log.py
+++ topics/emit_log.py
@@ -8,7 +8,7 @@ channel = connection.channel()
 channel.exchange_declare(exchange='topic_logs', exchange_type='topic')
 
 routing_key = sys.argv[1] if len(sys.argv) > 2 else 'anonymous.info'
-message = ' '.join(sys.argv[2:]) or 'Hello World!'
+message = ' '.join(sys.argv[2:]) or 'Hello World!, hi vicky'
 channel.basic_publish(
     exchange='topic_logs', routing_key=routing_key, body=message)
 print(" [x] Sent %r:%r" % (routing_key, message))
