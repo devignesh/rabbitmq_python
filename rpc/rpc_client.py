393ff52f2a5efbc1b7f1488e603f59d13d69d3d2
devignesh <vinesh1865@gmail.com>
2019-10-21 19:16:23 +0530

    client

--- rpc/rpc_client.py
+++ rpc/rpc_client.py
@@ -26,7 +26,7 @@ class FibonacciRpcClient(object):
         self.corr_id = str(uuid.uuid4())
         self.channel.basic_publish(
             exchange='',
-            routing_key='rpc_queue',
+            routing_key='rpc_queuee',
             properties=pika.BasicProperties(
                 reply_to=self.callback_queue,
                 correlation_id=self.corr_id,
